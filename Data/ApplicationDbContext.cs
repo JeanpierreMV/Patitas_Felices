25e246464475802c8c3d98d9d56168d587afc8a1
NIKOLELACUNZA <nikole_lacunza@usmp.pe>
2023-09-19 12:06:20 -0500

    db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Patitas_Felices.Models;
 
 namespace Patitas_Felices.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<MASCOTAS>? PRODUCTs{get; set;}
 }
+
---
+++ Models/CLIENTE.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Patitas_Felices.Models
+{
+     [Table("Cliente")]
+    public class CLIENTE
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string apodo { get; set; }
+        public string domicilio { get; set; }
+        public int celular { get; set; }
+        public int dni { get; set; }
+         public IdentityUser User { get; set; }
+    }
+}
