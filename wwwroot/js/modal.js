64918ed0d4d8029def29c59b9569b402064619a4
Jeanpierre Montalvo Velasquez <jeanpierre_montalvo@usmp.pe>
2023-10-06 21:52:06 -0500

    popupCommit

---
+++ wwwroot/js/modal.js
@@ -0,0 +1,16 @@
+let openModal = document.getElementById('openModal');
+let modalBit = document.getElementById('modal')
+let closeModal = document.getElementById('close');
+
+openModal.onclick = function(){
+    modalBit.style.visibility = 'visible';
+
+}
+
+openModal.onclick = function(){
+    modalBit.style.visibility = 'hidden';
+}
+
+modal_bit.onclick = function(){
+    modalBit.style.visibility = 'hidden';
+}
