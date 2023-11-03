4ea9eb034e880967b4592b59c8a1b3e5cde2841b
NIKOLELACUNZA <nikole_lacunza@usmp.pe>
2023-11-02 20:36:19 -0500

    viewsoliCommit

--- Controllers/DonacionesController.cs
+++ Controllers/DonacionesController.cs
@@ -1,15 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Patitas_Felices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Patitas_Felices.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Patitas_Felices.Controllers;
 
 public class DonacionesController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public DonacionesController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _context;
+    
+    public DonacionesController(ApplicationDbContext context,ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
@@ -17,7 +29,10 @@ public class DonacionesController : Controller
     {
         return View();
     }
-
+    public IActionResult VozAnimal()
+    {
+        return View();
+    }
      public IActionResult Suministros()
     {
         return View();
